22fb958de96847e5459cd32876cc46469365f9a5
Daniela <daniela.houck@etec.sp.gov.br>
2020-11-07 00:39:46 -0300

    Mudei a página sobre nós, pscanálise e João e Maria

--- src/cont_Joao_e_Maria.php
+++ src/cont_Joao_e_Maria.php
@@ -13,7 +13,7 @@
 
     <div style="width:100%;height:auto;">
 
-    <img src="<?php echo $conto.'img (44).jpg' ?>" class="" style="width:100%;height:430px;object-fit: cover;object-position: center 32%;"> 
+    <img src="<?php echo $conto.'img (41).jpg' ?>" class="" style="width:100%;height:430px;object-fit: cover;object-position: center 42%;"> 
 
     <div class="container-fluid mt-4  wt-geral  position-relative" style="min-height:81vh;">
 
@@ -32,7 +32,7 @@
         <p class="text-justify mb-4 font-s-11 recuo"><span class="font-london font-s-30 mr-2 px-3 border switch-cap text-white muda" style="line-height: 1;">A</span>i daquele que pensa em encontrar uma governanta para seus filhos dando-lhes uma madrasta! Ele apenas traz para sua casa a causa de sua ruína. Nunca houve uma madrasta que olhasse com benevolência para os filhos de outra pessoa; ou se por acaso alguém assim fosse encontrado, ela seria considerada um milagre e seria chamada de corvo branco. Mas, além de todos aqueles de quem você pode ter ouvido, vou agora lhe contar sobre outra, para ser adicionada à lista das madrastas sem coração, que você considerará bem merecedoras do castigo que ela comprou para si com dinheiro disponível.</p>
         <div class="row mb-4">
 
-            <div class="col-12 col-md-4" ><div class="bg-img imgc-size" style="background-image: url(img/cont/img.jpg);width:100%;height:100%;"></div></div>
+            <div class="col-12 col-md-4" ><div class="bg-img imgc-size" style="background-image: url(img/cont/img20.jpg);width:100%;height:100%;"></div></div>
             <div class="col-12 col-md-8 text-justify font-s-11"> 
               <p class="mb-4 recuo">Era uma vez um bom homem chamado Jannuccio, que tinha dois filhos, Nennillo e Nennella, a quem amava tanto quanto sua própria vida. Mas tendo a Morte, com a lima fila do Tempo, cortado as grades da prisão da alma de sua esposa, ele tomou para si uma mulher cruel, que mal havia posto os pés em sua casa, ela começou a montar no cavalo alto, dizendo:</p> 
               <p class="mb-4 recuo"> "Vim aqui, de fato, para cuidar dos filhos de outras pessoas? Um belo trabalho que empreendi, para ter todos esses problemas e ser incomodado para sempre por um casal de pirralhos gritando! Quem dera eu tivesse quebrado o pescoço antes de vir a este lugar, ter comida ruim, bebida pior e não dormir à noite! Aqui está uma vida para levar! Certamente, vim como uma esposa, não como uma serva; mas devo encontrar um meio de me livrar dessas criaturas, ou me custou minha vida: melhor corar uma vez do que ficar pálido cem vezes; portanto, acabei com eles, pois estou decidido a mandá-los embora ou a sair de casa para sempre."</p>
@@ -44,7 +44,7 @@
 
             
           <div class="col-12 col-md-8 text-justify font-s-11"> <p class="mb-4 recuo">O pobre marido, que tinha alguma afeição por esta mulher, disse-lhe: "Calma, mulher! Não te zangues, porque o açúcar é caro; e amanhã de manhã, antes que o galo cante, vou tirar este aborrecimento para agradar vocês." <br> Então, na manhã seguinte, antes que a Aurora pendurasse a colcha vermelha na janela do Leste para arejá-la, Jannuccio pegou as crianças, uma por cada mão, e com um bom cesto cheio de coisas para comer no braço, conduziu-as para um bosque, onde um exército de choupos e faias mantinham as sombras sitiadas.<br> Então Jannuccio disse: "Meus filhinhos, fiquem aqui neste bosque, e comam e bebam alegremente; mas se vocês quiserem alguma coisa, sigam esta linha de cinzas que eu tenho espalhado enquanto avançávamos; esta será uma pista para guiá-los sair do labirinto e levá-lo direto para casa."</p> </div>
-          <div class="col-12 col-md-4" ><div class="bg-img imgc-size" style="background-image: url(img/cont/img1.png);width:100%;height:100%;"></div></div>
+          <div class="col-12 col-md-4" ><div class="bg-img imgc-size" style="background-image: url(img/cont/img43.jpg);width:100%;height:100%;"></div></div>
             
 
         </div>
@@ -61,12 +61,49 @@
             </div>
         </div>
 
-        <p class="text-justify mb-4 font-s-11 recuo">texto</p>
-		<p class="text-justify mb-4 font-s-11 recuo">texto</p>
-		<p class="text-justify mb-4 font-s-11 recuo">texto</p>
+        <p class="text-justify mb-4 font-s-11 recuo">Mas como o Céu sempre estende seu braço sobre os inocentes, veio por acaso um príncipe para caçar naquela floresta. Então Nennillo, ouvindo o latido dos cães, ficou tão assustado que se esgueirou até uma árvore oca; e Nennella saiu correndo a toda velocidade, e correu até sair da floresta e se encontrar na praia. Aconteceu que alguns piratas, que haviam pousado ali para buscar combustível, viram Nennella e a carregaram; e seu capitão a levou para casa com ele, onde ele e sua esposa, tendo acabado de perder uma menina, a tomaram como sua filha.</p>
+		<p class="text-justify mb-4 font-s-11 recuo">Nesse ínterim, Nennillo, que se escondera na árvore, foi cercado pelos cães, que latiram tão furiosamente que o príncipe mandou descobrir a causa; e quando ele descobriu o lindo menino, que era tão jovem que ele não sabia quem eram seu pai e sua mãe, ele ordenou a um dos caçadores que o colocasse em sua sela e o levasse ao palácio real. Então, ele o educou com muito cuidado e instruiu-o em várias artes e, entre outras, ensinou-o a de escultor; de modo que, antes que se passassem três ou quatro anos, Nennillo se tornou tão especialista em sua arte que era capaz de esculpir um fio de cabelo.</p>
+		<p class="text-justify mb-4 font-s-11 recuo">Mais ou menos nessa época, foi descoberto que o capitão do navio que levara Nennella para sua casa era um ladrão do mar, e o povo queria prendê-lo; mas, recebendo notificação oportuna dos escrivães nos tribunais, que eram seus amigos e a quem ele pagava, ele fugiu com toda a família. Foi decretado, entretanto, talvez pelo julgamento do Céu, que aquele que cometeu seus crimes no mar, no mar, deveria sofrer a punição deles; por ter embarcado em um pequeno barco, mal estava em mar aberto, veio uma tal tempestade de vento e tumulto das ondas, que o barco se virou e todos se afogaram, todos exceto Nennella, que não tinha participado os roubos do corsário, como sua esposa e filhos, escaparam do perigo; para então um grande peixe encantado.</p>
         
+		  		  
+		  
+
+		<div class="row mb-4">
+
+          <div class="col-12 col-md-8 text-justify font-s-11"> 
+		  <p class="mb-4 recuo">A menina agora pensava que seus dias certamente haviam chegado ao fim, quando de repente ela encontrou algo que a surpreendeu dentro dos peixes: belos campos e belos jardins, e uma esplêndida mansão, com tudo que seu coração poderia desejar, na qual ela viveu como uma princesa. Em seguida, ela foi carregada rapidamente pelo peixe até uma rocha, onde por acaso o príncipe tinha vindo para escapar do calor escaldante do verão e desfrutar da brisa fresca do mar. E enquanto um grande banquete se preparava, Nennillo havia pisado em uma sacada do palácio na rocha para afiar algumas facas, orgulhando-se muito de ter conquistado honra em seu cargo. Quando Nennella o viu pela garganta do peixe, ela gritou alto: <br> <br> - Irmão, irmão, sua tarefa está cumprida, as mesas estão dispostas cada uma; <br> - Mas aqui no peixe devo sentar-me e suspirar, <br> - Oh irmão, sem você eu logo morrerei.</p> 
+		  <p class="mb-4 recuo">Nennillo a princípio não deu atenção à voz, mas o príncipe, que estava em outra sacada e também a ouvira, virou-se na direção de onde vinha o som e viu o peixe. E quando ele ouviu novamente as mesmas palavras, ele ficou fora de si de espanto, e ordenou que vários servos tentassem de alguma forma pegar o peixe e puxá-lo para a terra.</p>
+		  
+		  </div>
+          <div class="col-12 col-md-4" ><div class="bg-img imgc-size" style="background-image: url(img/cont/img1.png);width:100%;height:100%;"></div></div>
+            
+        </div>
+				
 		
-		<p class="text-justify mb-5 font-s-11 recuo">Depois de haver destinado uma grande renda ao cozinheiro, e de havê-lo nomeado camareiro-mor do palácio, o rei se casou com Talia, que viveu feliz e contente para sempre com seu marido e filhos, depois de ter experimentado que até mesmo dormindo é possível ser favorecida com a sorte.</p>
+		
+		<p class="text-justify mb-4 font-s-11 recuo"> Por fim, ouvindo as palavras "Irmão, irmão!" repetido continuamente, ele perguntou a todos os seus servos, um por um, se algum deles havia perdido uma irmã. E Nennillo respondeu que se lembrava, como um sonho, de ter tido uma irmã quando o príncipe o encontrou na floresta, mas que nunca mais tinha ouvido falar dela. Então o príncipe disse-lhe que se aproximasse do peixe e visse o que estava acontecendo, pois talvez essa aventura pudesse interessá-lo. </p>
+		
+		<p class="text-justify mb-4 font-s-11 recuo"> Assim que Nennillo se aproximou do peixe, ele ergueu a cabeça sobre a rocha e, abrindo a garganta com seis palmos de largura, Nennella saiu, tão bela que parecia uma ninfa em algum interlúdio, saiu daquele animal no encantamento de um mágico. E quando o príncipe lhe perguntou como tudo havia acontecido, ela lhe contou uma parte de sua triste história e o ódio de sua madrasta; mas não sendo capaz de lembrar o nome de seu pai nem de sua casa, o príncipe fez uma proclamação ser emitida, ordenando que quem tivesse perdido dois filhos, chamados Nennillo e Nennella, em um bosque, deveria vir ao palácio real, e ele iria receber notícias alegres deles. </p>
+		
+		<p class="text-justify mb-4 font-s-11 recuo"> Jannuccio, que durante todo esse tempo passara uma vida triste e desconsolada, acreditando que seus filhos haviam sido devorados por lobos, apressou-se agora com a maior alegria em procurar o príncipe e lhe disse que havia perdido os filhos. E quando ele contou a história, como ele foi compelido a levá-los para a floresta, o príncipe deu-lhe uma boa repreensão, chamando-o de estúpido por permitir que uma mulher colocasse o calcanhar em seu pescoço até que ele fosse levado para mandar embora duas joias como seus filhos. Mas depois de ter quebrado a cabeça de Jannuccio com essas palavras, aplicou nela o emplastro da consolação, mostrando-lhe os filhos, que o pai abraçou e beijou por meia hora sem se dar por satisfeito. </p>
+		
+		
+			
+		
+		 <div class="row mb-4">
+
+            <div class="col-12 col-md-4" ><div class="bg-img imgc-size" style="background-image: url(img/cont/img2.jpg);width:100%;height:100%;background-position: center 70%;"></div></div>
+            <div class="col-12 col-md-8 text-justify font-s-11"> 
+              <p class="mb-4 recuo"> Então o príncipe o fez tirar o paletó e o vestiu como um senhor; e mandando chamar a mulher de Jannuccio, mostrou-lhe aquelas duas pepitas de ouro, perguntou-lhe o que merecia aquela pessoa que lhes fizesse mal e até colocasse em perigo a vida deles.</p>
+              <p class="mb-4 recuo">E ela respondeu: "De minha parte, eu a colocaria em um barril fechado e a faria rolar montanha abaixo." <br> "Assim deve ser feito!" disse o príncipe. "A cabra deu uma cabeçada em si mesma. Rápido agora! Você passou a sentença, e deve sofrer, por ter gerado tamanha malícia a esses lindos enteados."</p>
+		  
+		   </div>
+        </div>
+		
+		
+		
+		
+		<p class="text-justify mb-5 font-s-11 recuo">Então ele deu ordens para que a sentença fosse executada imediatamente. Então, escolhendo um senhor muito rico entre seus vassalos, deu-lhe Nennella por esposa, e a filha de outro grande senhor para Nennillo; permitindo-lhes viver o suficiente, com seu pai, de modo que eles não desejassem nada no mundo. Mas a madrasta, encerrada no barril e excluída da vida, continuou a chorar através do buraco enquanto teve fôlego: <br><br> - Àquele que o mal busca, o mal cairá; <br> - Chega uma hora que recompensa a todos.</p>
 
         <center><h1 class="font-antic mt-3 mb-2">Fim</h1></center>
         
--- src/cont_Psicanalise.php
+++ src/cont_Psicanalise.php
@@ -16,7 +16,7 @@
     <div class="row position-absolute mt-5" style="width:100%;">
       <div class="col-1 col-md-3"></div>
       <div class="col-10 col-md-6 mt-5">
-        <center><h1 class="mt-5 text-center font-antic font-s-35 text-white">A psicanalise por trás dos contos de fadas</h1></center>
+        <center><h1 class="mt-5 text-center font-london font-s-40 text-white">A psicanálise por trás dos contos de fadas</h1></center>
       </div>
       <div class="col-1 col-md-3"></div>
     </div>
@@ -40,7 +40,7 @@
 
             <br>
         
-            <p class="mt-5 mb-4 text-justify font-s-13 recuo"> <span id="introducao" class="dest-purple muda">Os contos de fadas</span> possuem uma natureza ética e existencial, em muitas de suas histórias são retratadps heróis e heroínas, e durante essas narrativas, é representado, por meio desses personagens, a possível realização de um ideal ou sonho. Esses contos têm grande impacto na vida da criança, visto que transmitem entretenimento, despertam a imaginação e a curiosidade de seus ouvintes, leitores e espectadores, além de ajudálos a desenvolver seu intelecto e tornar claras suas emoções, possibilitando que aquele que tenha algum tipo de contato com os contos de fadas reconheça suas dificuldades e desenvolva soluções para os problemas que o aflinge.</p> 
+            <p class="mt-5 mb-4 text-justify font-s-13 recuo"><span id="introducao" class="dest-purple muda" style="font-family: Brush Script MT; font-size:1.5em;">Os contos de fadas</span> possuem uma natureza ética e existencial, em muitas de suas histórias são retratados heróis e heroínas, e durante essas narrativas, é representado, por meio desses personagens, a possível realização de um ideal ou sonho. Esses contos têm grande impacto na vida da criança, visto que transmitem entretenimento, despertam a imaginação e a curiosidade de seus ouvintes, leitores e espectadores, além de ajudálos a desenvolver seu intelecto e tornar claras suas emoções, possibilitando que aquele que tenha algum tipo de contato com os contos de fadas reconheça suas dificuldades e desenvolva soluções para os problemas que o aflinge.</p> 
         
         </div>
 
@@ -100,7 +100,7 @@
 
     <div class="container-fluid mt-0  wt-geral  position-relative" >
 
-            <center><h1 id="social" class="mt-1 mb-5 dest-purple muda font-antic">Os problemas sociais</h1></center>
+            <center><h1 id="social" class="mt-1 mb-5 dest-purple muda font-antic"><br>Os problemas sociais</h1></center>
         
             
 
@@ -113,13 +113,13 @@
 
               <p class="mt-1 mb-4 text-justify font-s-13 recuo"> Ressalva-se a importância dos contos para entender os problemas sociais existentes em nossa sociedade desde a antiguedade. Ao analisar os contos de fadas folclóricos nota-se elementos mais perversos do que os apresentados nas versões de Walter Elias Disney.</p> 
 
-              <p class="mt-1 mb-4 text-justify font-s-13 recuo"> Neles são abordados temas como o abando, o estupro, o incesto, o canibalismo, a antropofagia, a mutilação e a <span style="color:#f4b12f;">miséria</span>.</p>
+              <p class="mt-1 mb-4 text-justify font-s-13 recuo"> Neles são abordados temas como o abandono, o estupro, o incesto, o canibalismo, a antropofagia, a mutilação e a <span style="color:#f4b12f;">miséria</span>.</p>
 
               </div>
 
             </div>
 
-            <p class="mt-1 mb-4 text-justify font-s-13 recuo">A introdução a estes temas desde cedo ajuda a criança a questionar o porquê da existência desses problemas, desenvolvendo um senso crítico fundamental para as fases seguintes de sua vida, auxiliando na criação de soluções para problemas futuros. Isso ocorre devido aos contos de fada ensinarem que quaisquer que sejam as dificuldades da vida, elas devem ser enfrentadas para serem derrotadas, justamente por fazerem parte da essência humana.</p>
+            <p class="mt-1 mb-4 text-justify font-s-13 recuo">A introdução a estes temas desde cedo ajuda a criança a questionar o porquê da existência desses problemas, desenvolvendo um senso crítico fundamental para as fases seguintes de sua vida, auxiliando na criação de soluções para problemas futuros. Isso ocorre devido aos contos de fada ensinarem que quaisquer que sejam as dificuldades da vida, elas devem ser enfrentadas para serem derrotadas, justamente por fazerem parte da essência humana.</p><br>
         
         </div>
 
@@ -137,12 +137,12 @@
       <br>
       </div>
 
-      <div class="bg-img" style="width:100%;height:100%;background-image:url(img/bg/bgwolf.png);">
+      <div id="MoralEtica" class="bg-img" style="width:100%;height:100%;background-image:url(img/bg/bgwolf.png);">
       <br><br><br><br>
       
         <div class="container-fluid mt-5 mb-5  wt-geral ">
 
-        <center><h1 id="MoralEtica" class="mt-1 mb-5 muda font-antic" style="color:#CFBFFF">Moral e Ética</h1></center>
+        <center><h1 class="mt-1 mb-5 muda font-antic" style="color:#CFBFFF">Moral e Ética</h1></center>
 
         <p class="mt-1 mb-4 text-justify font-s-16 recuo text-white"> Outra função importante é a construção da moral e ética por meio dos contos de fada. De forma subentendida suas histórias passam as regras da sociedade; basta analisar os elementos presentes em sua narrativa e como eles simplificavam a realidade para que a mensagem fosse claramente entendida pelas crianças</p>
 
@@ -153,7 +153,7 @@
 
       <div class="container-fluid mt-0 mb-5  wt-geral  position-relative" >
 
-            <center><h1 id="Mudanca" class="mt-5 mb-5 dest-purple muda font-antic">Mudanças</h1></center>
+            <center><h1 id="Mudanca" class="mt-5 mb-5 dest-purple muda font-antic"><br>Mudanças</h1></center>
 
             <p class="mt-1 mb-4 text-justify font-s-13 recuo"> Ao decorrer do tempo foram tirados diversos elementos que avaliaram inadequados para as crianças(antigamente o público-alvo desses contos eram os adultos), primeiro tiraram qualquer conteúdo possivelmente sexual, e por fim, elementos que trouxessem violência e tabus.</p>
             <p class="mt-1 mb-4 text-justify font-s-13 recuo"> Como lembra Bettelheim (1980, p. 32), atualmente as crianças conhecem os contos de fadas de forma amesquinhas e simplificadas, que amortecem os significados e roubam-nas de todo o significado mais profundo.</p>
@@ -171,7 +171,7 @@
         <div class="container-fluid mt-5 mb-5  wt-geral ">
         
 
-        <center><h1 class="mt-5 mb-5 dest-purple muda font-antic">Sistema mentais</h1></center>
+        <center><h1 class="mt-5 mb-5 dest-purple muda font-antic"><br>Sistema mentais</h1></center>
 
         <p class="mt-1 mb-4 text-justify font-s-13 recuo">Aplicando o modelo psicanalítico da formação da personalidade humana (Teoria Topográfica de Freud), os contos de fadas transmitem importantes mensagens para os três sistemas mentais (A mente consciente, a pré-inconsciente e a inconsciente), seja em qualquer nível que esteja funcionando naquele momento.</p>
         <p class="mt-1 mb-4 text-justify font-s-13 recuo">Essas histórias abordam problemas humanos universais, em especial os que preocupam as crianças. Elas falam ao ego que desabrocha e encorajam o seu desenvolvimento, ao mesmo tempo que aliviam as pressões pré-conscientes e inconscientes. Aos desenrolar das histórias, é dado crédito consciente e corpo às pressões do id, mostrando caminhos para satisfazê-las que estão de acordo com as exigências do ego e do superego.</p>
@@ -183,7 +183,7 @@
       <div class="container-fluid mt-5 mb-5  wt-geral ">
         
 
-        <center><h1 id="leitura" class="mt-5 mb-5 dest-purple muda font-antic">A importância da leitura</h1></center>
+        <center><h1 id="leitura" class="mt-5 mb-5 dest-purple muda font-antic"><br>A importância da leitura</h1></center>
 
         <p class="mt-1 mb-4 text-justify font-s-13 recuo">A leitura dos contos de fada é importante para o desenvolvimento emocional e psicológico da criança, visto que ensinam a enfrentar suas dificuldades, estimulando-as a não se intimidarem, e passam a mensagem que nenhum problema é impossível. Seus enredos falam de sentimentos comuns a todos nós, como: ódio, inveja, ciúme, ambição, rejeição e frustação, que só podem ser compreendidos e vivenciados pela criança através das emoções e da fantasia.</p>
         <p class="mt-1 mb-4 text-justify font-s-13 recuo"> Por meio dela é possível que a criança aprenda a lidar com essas sensações. Além de que, o contato com os livros auxilia no desenvolvimento cognitivo e intelectual das crianças. Contribuindo também para melhorias no vocabulário, na fala, no rendimento escolar, na interpretação e no conhecimento.</p>
@@ -195,7 +195,7 @@
         <div class="container-fluid mt-5 mb-5  wt-geral ">
         
 
-        <center><h1 id="recomendacao" class="mt-5 mb-1 dest-purple muda font-antic">Recomendações</h1></center>
+        <center><h1 id="recomendacao" class="mt-5 mb-1 dest-purple muda font-antic"><br>Recomendações</h1></center>
 
         <div class="row">
           <div class="col-md-3"></div>
