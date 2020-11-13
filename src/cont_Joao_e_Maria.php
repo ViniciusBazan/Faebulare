dff38c23eff88801d64448546fb91220de7e19e0
Juh <julia.morais.oluveira@gmail.com>
2020-11-13 16:46:26 -0300

    arrumei as imagens e add o conto da ariel q coloquei como arial kkkkkkkk

--- src/cont_Joao_e_Maria.php
+++ src/cont_Joao_e_Maria.php
@@ -13,7 +13,7 @@
 
     <div style="width:100%;height:auto;">
 
-    <img src="<?php echo $conto.'img (41).jpg' ?>" class="" style="width:100%;height:430px;object-fit: cover;object-position: center 42%;"> 
+    <img src="<?php echo $conto.'JM_1.jpg' ?>" class="" style="width:100%;height:430px;object-fit: cover;object-position: center 42%;"> 
 
     <div class="container-fluid mt-4  wt-geral  position-relative" style="min-height:81vh;">
 
